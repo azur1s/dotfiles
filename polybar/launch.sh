7cac36eeb221d133cfcdbaa5ec7679a03534a9c8
Natapat Samutpong <natapat.samutpong@gmail.com>
2022-01-16 03:51:48 +0700

    polybar refactowor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar -c "~/.config/polybar/config.ini" main 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
